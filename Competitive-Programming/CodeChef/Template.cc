b80d68da61b5ffceaa0289fe0bf171b09e4564ab
Rohan Sarap <saraprohan@outlook.com>
2018-07-10 22:43:11 +0530

    Create Template.cc

---
+++ Competitive-Programming/CodeChef/Template.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define foreach(it, v) for(__typeof((v).begin()) it=(v).begin(); it != (v).end(); ++it)
+#define SYNC ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+using namespace std;
+
+void tr() {cout<<endl;}
+
+void solve() {    
+}
+
+int main() {
+    SYNC
+    int t;
+    cin>>t; 
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
