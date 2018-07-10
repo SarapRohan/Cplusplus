f932b37201ee4ecbb15e53010430a44c22fcdfb7
Rohan Sarap <saraprohan@outlook.com>
2018-07-10 23:15:40 +0530

    Update Template.cc

--- Competitive-Programming/CodeChef/Template.cc
+++ Competitive-Programming/CodeChef/Template.cc
@@ -6,13 +6,13 @@
 #define mp make_pair
 #define foreach(it, v) for(__typeof((v).begin()) it=(v).begin(); it != (v).end(); ++it)
 #define SYNC ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define MOD 1e9 + 7
-
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
+#define MOD 1e9+7
+#define LL long long
 
 using namespace std;
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
 
 void tr() {cout<<endl;}
 
