511d1429e263e0288f655bc3d5a20aa2faf3625f
Rohan Sarap <saraprohan@outlook.com>
2018-07-10 19:11:13 +0530

    Create RectangleArea.cc
    Largest Area of Rectangle in Histogram
    #Stack

---
+++ Competitive-Programming/RectangleArea.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+int largestRectangleArea(vector<int> v) {
+	stack<int> s;
+    int max_area=0,top,area_top;
+    int i=0;
+    while(i<A.size()) {
+        if(s.empty() || A[s.top()]<=A[i]) s.push(i++);
+        else {
+            top=s.top();
+            s.pop();
+            area_top=A[top]*(s.empty() ? i:i-s.top()-1);
+            max_area=max(max_area,area_top);
+        }
+    }
+    while(!s.empty()) {
+        top=s.top();
+        s.pop();
+        area_top=A[top]*(s.empty()?i:i-s.top()-1);
+        max_area=max(max_area,area_top);
+    }
+    return max_area;
+}
+int main() {
+	vector<int> v = {2, 1, 5, 6, 2, 3};
+	cout<<largestRectangleArea(v)<<endl;
+	return 0;
+}
