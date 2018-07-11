82c6ae715d8c64153330af57daaed7745507a034
Rohan Sarap <saraprohan@outlook.com>
2018-07-11 17:22:15 +0530

    Create SubsetSum.cc

---
+++ Competitive-Programming/SubsetSum.cc
@@ -0,0 +1,9 @@
+int subsetSum(vector<int> v, int s) {
+    int dp[s + ] = {0};
+    dp[0] = 1;
+    for(int n : nums) {
+        for(int i = s; i >= n; i--)
+            dp[i] += dp[i - n];
+    }
+    return dp[s]s;
+}
