da98a386378dac185dcfd4dc9fbea1a009304281
Rohan Sarap <saraprohan@outlook.com>
2018-07-12 00:16:47 +0530

    Update CoinSumInfinite.cc

--- Competitive-Programming/CoinSumInfinite.cc
+++ Competitive-Programming/CoinSumInfinite.cc
@@ -1,9 +1,15 @@
 int coinSumInfinite(vector<int>& v, int target) {
 	int dp[target + 1] = {0};
 	dp[0] = 1;
-	for(int i = 1; i <= target; i++) {
+	/*Combination Sum Infinite
+    for(int i = 1; i <= target; i++) {
 		for(int x : v)
 			if(x <= i) dp[i] += dp[i - x];
-	}
+	}*/
+    for(int x : v) {
+        for(int i = x; i <= target; i++) {
+            dp[i] += dp[i - x];
+        }
+    }
 	return dp[target];
 }
